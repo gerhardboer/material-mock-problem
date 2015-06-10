d721057550f77389b945f97f9ba36df91c7af1ed
Gerhard Boer <gghboer@gmail.com>
2015-06-10 19:09:54 +0200

    Initial commit

---
+++ GruntFile.js
@@ -0,0 +1,118 @@
+// Generated on 2015-01-29 using generator-angular 0.10.0
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+
+  // Configurable paths for the application
+  var appConfig = {
+    app: 'app'
+  };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    yeoman: appConfig,
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      js: {
+        files: ['<%= yeoman.app %>/src/{,*/}*.js'],
+        tasks: [''],
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        }
+      },
+      styles: {
+        files: ['<%= yeoman.app %>/assets/{,*/}*.css'],
+        tasks: ['newer:copy:styles']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= yeoman.app %>/src/**/*.js',
+          '<%= yeoman.app %>/{,*/}*.html',
+          '<%= yeoman.app %>/src/**/*.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          middleware: function (connect) {
+            return [
+              connect.static('.tmp'),
+              connect().use(
+                '/bower_components',
+                connect.static('./bower_components')
+              ),
+              connect.static(appConfig.app)
+            ];
+          }
+        }
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            //'.htaccess',
+            '*.html',
+            'resources/json/*.json',
+            '*/{,*/}*.html',
+            'images/{,*/}*.{webp}',
+            'fonts/{,*/}*.*'
+          ]
+        }, {
+          expand: true,
+          cwd: '.tmp/images',
+          dest: '<%= yeoman.dist %>/images',
+          src: ['generated/*']
+        }]
+      },
+      styles: {
+        expand: true,
+        cwd: '<%= yeoman.app %>/resources/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    }
+  });
+
+
+  grunt.registerTask('serve', [
+    'connect:livereload',
+    'watch'
+  ]);
+};
